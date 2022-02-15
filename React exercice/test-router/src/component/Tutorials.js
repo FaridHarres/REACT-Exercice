0cef1e64ba7ecd70401993382b3c845b7fc9b76c
FaridHarres <farid.harres@gmail.com>
2022-02-15 10:50:23 +0100

    recherche fiche via id et axios

--- React exercice/test-router/src/component/Tutorials.js
+++ React exercice/test-router/src/component/Tutorials.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Tutorial = (props) => {
+    //via ce props on a eu acces a 3 objet uniquement si cest route
     //au bout de 5 seconde si la page ne se charge pas il sera redirigé
     setTimeout(() => {
         props.history.push('/')
@@ -8,7 +9,7 @@ const Tutorial = (props) => {
 
     return (
 
-        <div classNameName="container mt-3">
+        <div classNameName="container mt-3 ">
             <div class="alert alert-warning alert-dismissible fade show" role="alert">
                 <strong>OUPSSSSSS</strong> This page is under construction you will be redirected to the docs page in 5 seconds
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
