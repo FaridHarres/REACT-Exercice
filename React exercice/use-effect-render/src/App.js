543999c82b47f61cd682a6f95f47d0470936d89e
FaridHarres <farid.harres@gmail.com>
2022-02-16 10:13:34 +0100

    UseEffect render

---
+++ React exercice/use-effect-render/src/App.js
@@ -0,0 +1,14 @@
+// import ClassCount from './components/ClassCount';
+import './App.css';
+import FunctionCount from './components/FunctionCount';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCount/> */}
+      <FunctionCount/>
+    </div>
+  );
+}
+
+export default App;
